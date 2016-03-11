07b01c86cf8efd99c19c048069d857981b3b6990
V.Dheeraj Thany Swarup <dheerajthany2@gmail.com>
2016-03-11 13:18:52 +0530

    validation completed

--- rails/toy_app/app/models/user.rb
+++ rails/toy_app/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	validates :name, length: { maximum: 24 }, presence: true
+	validates :email, presence: true
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
